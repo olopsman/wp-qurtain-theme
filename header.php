1804686e9129525db6e9d7ac0fa37c69f47562eb
Paulo Orquillo <paulo.orquill@gmail.com>
2017-10-07 20:35:13 +1300

    Removed classie.js and used simple jquery toggleClass for navigation menu

--- header.php
+++ header.php
@@ -6,6 +6,10 @@
 		<script>
 		   jQuery(document).ready(function(){
 				 jQuery('iframe').wrap("<div class='iframe-flexible-container'></div>");
+				 //select the mobile nav
+				 jQuery('#burger-nav').on("click", function() {
+					 	jQuery('.menu-wrap ul').toggleClass('open');
+				 });
 		   });
 		</script>
 	</head>
@@ -28,7 +32,7 @@
 						<div class="custom-logo">
 						<?php the_custom_logo(); ?>
 						</div>
-	          <a id="nav-open-button" class="nav-button gray hide-md">
+	          <a id="burger-nav" class="nav-button gray hide-md">
 	              <svg version="1.1" class="button-svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="24px" height="23px" viewBox="0 0 24 23" enable-background="new 0 0 24 23" xml:space="preserve">
 	      		<path class="fill" d="M21,6H3C2.171,6,1.5,5.329,1.5,4.5S2.171,3,3,3h18c0.828,0,1.5,0.671,1.5,1.5S21.828,6,21,6z M22.5,11.5
 	          c0-0.829-0.672-1.5-1.5-1.5H3c-0.829,0-1.5,0.671-1.5,1.5C1.5,12.328,2.171,13,3,13h18C21.828,13,22.5,12.328,22.5,11.5z M22.5,18.5
@@ -37,7 +41,6 @@
 	          </a>
 						<div class="menu-wrap">
 						<?php wp_nav_menu( array( 'theme_location' => 'primary') ); ?>
-						<button class="close-button" id="close-button">Close Menu</button>
 	        </div>
 				</nav>
 				<!-- end mobile -->
